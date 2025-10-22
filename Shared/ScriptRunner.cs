9777958ac1649986d46f17a98fbe7ff7cd6d5ace
Martin Hafner <hafner.martin.hm@gmail.com>
2025-10-22 21:11:06 +0200

    Update ScriptRunner.cs

--- Shared/ScriptRunner.cs
+++ Shared/ScriptRunner.cs
@@ -7,23 +7,31 @@ using Microsoft.CodeAnalysis.Scripting;
 
 namespace SharpFarm.Shared;
 
-public class ScriptGlobals
+public class ScriptRunner
 {
-    public GameWorld world { get; set; }
-}
-
-public class ScriptRunner {
-    public async Task<string> RunAsync(string code, GameWorld world) {
-        var codeWithLog = "using System;\n" + code + "\nConsole.WriteLine(\"Script ran\");";
+    public async Task<string> RunAsync(string code, GameWorld world)
+    {
         var options = ScriptOptions.Default
             .AddReferences(typeof(GameWorld).Assembly)
             .AddImports("System", "System.Collections.Generic", "SharpFarm.Shared");
-        var globals = new ScriptGlobals { world = world };
-        try {
-            await CSharpScript.EvaluateAsync(code, options, globals: globals);
+
+        try
+        {
+            // globals-Anonyme Typen in Blazor WASM funktionieren nicht zuverlässig.
+            // Wir übergeben world über eine bekannte Variable
+            string wrappedCode = $"var world = (SharpFarm.Shared.GameWorld)DotNet.invokeMethod('SharpFarm', 'GetWorld');\n{code}";
+
+            // In Blazor WebAssembly ggf. roslyn scripting DLLs müssen im wwwroot/roslyn liegen
+            await CSharpScript.EvaluateAsync(wrappedCode, options);
             return "OK";
-        } catch (Exception ex) {
+        }
+        catch (Exception ex)
+        {
             return ex.Message;
         }
     }
+
+    // Für DotNet.invokeMethodAsync in JS
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public static GameWorld GetWorld() => GameWorld.Instance;
 }
